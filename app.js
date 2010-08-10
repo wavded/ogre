4d545a2fceac0877c0f8354c33eb82950d91d506
Marc Harter <wavded@gmail.com>
2010-08-10 08:18:40 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var app = require('express').createServer(),
+    helper = require('./helper'),
+    connect = require('connect'),
+    ejs = require('ejs');
+
+app.configure(function(){
+    app.use(connect.staticProvider(__dirname + '/public'));
+});
+
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res){
+    res.render('home')
+})
+
+app.post('/convert', function(req, res){
+    helper.upload(req,function(outputstream){
+        if(outputstream){
+            res.header("Content-Type","application/json")
+            res.send(outputstream) }
+        else res.send("Unable to perform transformation")
+    })
+})
+
+app.error(function(err, req, res, next){
+    res.send(err.message, 500)
+})
+
+app.listen(3000);
+
---
+++ helper.js
@@ -0,0 +1,104 @@
+var ex = require('child_process').exec,
+    fm = require('formidable'),
+    fs = require('fs'),
+    sys = require('sys'),
+
+    handleRequest = function(req,callback){
+        var form = new fm.IncomingForm()
+
+        form.parse(req, function(err, fields, files) {
+            var file = files.upload || {};
+            file.callback = callback
+            file.jsonCallbackFn = fields.callback
+
+            err || !file.filename ? handleFileError(err,file) : processFile(file)
+        })
+    },
+
+    handleFileError = function(err,file) {
+        file.error = "Ogre needs a file to work with"
+        handleError(err,file)
+    },
+
+    processFile = function(file) {
+        file.ext = file.filename.replace(/^.*\.([A-Za-z0-9_]+)$/,"$1")
+        file.outputfile = file.path + ".json"
+        file.ext == "zip" ? handleZip(file) : handleSingle(file)
+    },
+
+    handleSingle = function(file) {
+        file.inputfile = file.path + "." + file.ext
+        fs.rename(file.path,file.inputfile,function(){
+            runOgre(file)
+        })
+    },
+
+    handleZip = function(file) {
+        file.zipDirectory = file.path + '_zip'
+        ex('unzip ' + file.path + ' -d ' + file.zipDirectory,
+            function(err,stdout){
+                file.inputfile = stdout.match(/inflating: (.*.shp)/)[1]
+                runOgre(file)
+            }
+        )
+    },
+
+    runOgre = function(file) {
+        ex('ogr2ogr -f "GeoJSON" stdout ' + file.inputfile, {maxBuffer: 1024 * 800},
+            function(err,stdout,stderr){
+                file.outputstream = stdout
+                err ? handleOgreError(err,file) : handleOgreSuccess(file)
+            }
+        )
+    },
+
+    handleOgreError = function(err,file) {
+        file.error = "Ogre couldn't convert this file"
+        handleError(err,file)
+    },
+
+    handleOgreSuccess = function(file) {
+        sys.log(file.inputfile)
+        file.jsonCallbackFn ? handleJsonCallback(file) : handleCallback(file)
+    },
+
+    handleError = function(err,file){
+        sys.log(err)
+        file.outputstream = JSON.stringify({
+            filename: file.filename,
+            error: "Ogre couldn't convert this file",
+            message: err.message
+        })
+        file.jsonCallbackFn ? handleJsonCallback(file) : handleCallback(file)
+    },
+
+    handleCallback = function(file) {
+        file.callback(file.outputstream)
+        file.zipDirectory ? cleanupZip(file) : cleanupSingle(file)
+    },
+
+    handleJsonCallback = function(file) {
+        file.outputstream = file.jsonCallbackFn + "(" + file.outputstream + ")"
+        file.callback(file.outputstream)
+        file.zipDirectory ? cleanupZip(file) : cleanupSingle(file)
+    },
+
+    cleanupSingle = function(file){
+        fs.unlink(file.inputfile)
+    },
+
+    cleanupZip = function(file){
+        fs.readdir(file.zipDirectory,function(err,files){
+            var len = files.length,
+                completed = 0
+            while(len--)
+                fs.unlink(file.zipDirectory + "/" + files[len],function(){
+                    completed++
+                    if(completed == files.length) fs.rmdir(file.zipDirectory)
+                })
+        })
+        fs.unlink(file.path)
+    }
+
+exports.upload = handleRequest
+
