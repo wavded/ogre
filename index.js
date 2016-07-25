3a15579914057dd6e4faf26c81b139991c8152a8
Marc Harter <wavded@gmail.com>
2016-07-25 10:44:32 -0500

    Lint

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var ogr2ogr = require('ogr2ogr')
 var fs = require('fs')
 var urlencoded = require('body-parser').urlencoded
 
-function enableCors (req, res, next) {
+function enableCors(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'POST')
   res.header('Access-Control-Allow-Headers', 'X-Requested-With')
@@ -12,24 +12,26 @@ function enableCors (req, res, next) {
   next()
 }
 
-function optionsHandler (methods) {
-  return function (req, res, next) {
+function optionsHandler(methods) {
+  return function(req, res) {
     res.header('Allow', methods)
     res.send(methods)
   }
 }
 
 function isOgreFailureError(er) {
-  return er && er.message && er.message.indexOf('FAILURE:') !== -1;
+  return er && er.message && er.message.indexOf('FAILURE:') !== -1
 }
 
 function safelyParseJson(json) {
   try {
     if (json) return JSON.parse(json)
-  } catch (e) { }
+  } catch (e) {
+    return ''
+  }
 }
 
-exports.createServer = function (opts) {
+exports.createServer = function(opts) {
   if (!opts) opts = {}
 
   var app = express()
@@ -40,15 +42,18 @@ exports.createServer = function (opts) {
   app.options('/convertJson', enableCors, optionsHandler('POST'))
 
   app.use(express.static(__dirname + '/public'))
-  app.get('/', function (req, res) {
+  app.get('/', function(req, res) {
     res.render('home')
   })
 
-  app.use(urlencoded({ extended: false, limit: 3000000 }))
+  app.use(urlencoded({extended: false, limit: 3000000}))
   app.use(multiparty())
 
-  app.post('/convert', enableCors, function (req, res, next) {
-    if (!req.files.upload || !req.files.upload.name) return res.status(400).json({ error: true, msg: 'No file provided' })
+  app.post('/convert', enableCors, function(req, res, next) {
+    if (!req.files.upload || !req.files.upload.name) {
+      res.status(400).json({error: true, msg: 'No file provided'})
+      return
+    }
 
     var ogr = ogr2ogr(req.files.upload.path)
 
@@ -64,12 +69,19 @@ exports.createServer = function (opts) {
       ogr.timeout(opts.timeout)
     }
 
-    res.header('Content-Type', 'forcePlainText' in req.body ? 'text/plain; charset=utf-8' : 'application/json; charset=utf-8')
+    res.header('Content-Type',
+      'forcePlainText' in req.body
+        ? 'text/plain; charset=utf-8'
+        : 'application/json; charset=utf-8'
+    )
 
-    ogr.exec(function (er, data) {
+    ogr.exec(function(er, data) {
       fs.unlink(req.files.upload.path)
 
-      if (isOgreFailureError(er)) return res.status(400).json({ errors: er.message.replace('\n\n','').split('\n') })
+      if (isOgreFailureError(er)) {
+        return res.status(400).json({errors: er.message.replace('\n\n','').split('\n')})
+      }
+
       if (er) return next(er)
 
       if (req.body.callback) res.write(req.body.callback + '(')
@@ -79,12 +91,12 @@ exports.createServer = function (opts) {
     })
   })
 
-  app.post('/convertJson', enableCors, function (req, res, next) {
-    if (!req.body.jsonUrl && !req.body.json) return res.status(400).json({ error: true, msg: 'No json provided' })
+  app.post('/convertJson', enableCors, function(req, res, next) {
+    if (!req.body.jsonUrl && !req.body.json) return res.status(400).json({error: true, msg: 'No json provided'})
 
-    var json = safelyParseJson(req.body.json);
-    
-    if (req.body.json && !json) return res.status(400).json({ error: true, msg: 'Invalid json provided' })
+    var json = safelyParseJson(req.body.json)
+
+    if (req.body.json && !json) return res.status(400).json({error: true, msg: 'Invalid json provided'})
 
     var ogr
 
@@ -109,8 +121,8 @@ exports.createServer = function (opts) {
 
     var format = req.body.format || 'shp'
 
-    ogr.format(format).exec(function (er, buf) {
-      if (isOgreFailureError(er)) return res.status(400).json({ errors: er.message.replace('\n\n','').split('\n') })
+    ogr.format(format).exec(function(er, buf) {
+      if (isOgreFailureError(er)) return res.status(400).json({errors: er.message.replace('\n\n','').split('\n')})
       if (er) return next(er)
       res.header('Content-Type', 'application/zip')
       res.header('Content-Disposition', 'filename=' + (req.body.outputName || 'ogre.zip'))
@@ -118,10 +130,11 @@ exports.createServer = function (opts) {
     })
   })
 
-  app.use(function (er, req, res, next) {
+  /* eslint no-unused-vars: [0, { "args": "none" }] */
+  app.use(function(er, req, res, next) {
     console.error(er.stack)
     res.header('Content-Type', 'application/json')
-    res.status(500).json({ error: true, msg: er.message })
+    res.status(500).json({error: true, msg: er.message})
   })
 
   return app
--- run.js
+++ run.js
@@ -1,3 +1,3 @@
-var ogre = require('./');
+var ogre = require('./')
 ogre.createServer().listen(3000)
 console.log('Ogre listening on port 3000')
